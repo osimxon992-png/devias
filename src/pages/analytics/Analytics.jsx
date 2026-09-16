2905c33112b336c499ea6b216437686022601233
Osimxon <osimhondadaxanov2013@gmail.com>
2026-09-16 22:30:02 +0500

    fixed

--- src/pages/analytics/Analytics.jsx
+++ src/pages/analytics/Analytics.jsx
@@ -4,7 +4,7 @@ import MainAnalytics from "../../components/mainAnalytics/MainAnalytics";
 function Analytics() {
   return (
     <div>
-      <section className="w-full h-auto min-h-[1546px] p-[64px_25px]">
+      <section className="w-full h-auto min-h-[1146px] p-[64px_25px]">
         <AnalyticsHeader />
         <MainAnalytics />
       </section>
