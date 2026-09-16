53a300da8721bd059b13e4c5da43b23bacac3e8b
Osimxon <osimhondadaxanov2013@gmail.com>
2026-09-16 21:18:30 +0500

    djkch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import EditedSidebar from "./components/editedSidebar/EditedSidebar";
-import Account from "./pages/Account";
+import Account from "./pages/account/Account";
 import Header from "./components/header/Header";
-import BlogOne from './pages/blogOne/BlogOne';
-import BlogTwo from './pages/blogTwo/BlogTwo';
-import BlogThree from './pages/blogThree/BlogThree';
-import SocialMeadiaOne from './pages/socialMediaOne/SocialMeadiaOne';
-import SocialMediaTwo from './pages/socialMediaTwo/SocialMediaTwo';
+import BlogOne from "./pages/blogOne/BlogOne";
+import BlogTwo from "./pages/blogTwo/BlogTwo";
+import BlogThree from "./pages/blogThree/BlogThree";
+import SocialMeadiaOne from "./pages/socialMediaOne/SocialMeadiaOne";
+import SocialMediaTwo from "./pages/socialMediaTwo/SocialMediaTwo";
 
 function App() {
   return (
@@ -18,11 +18,11 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Account />} />
-          <Route path="/blogOne" element={<BlogOne/>}/>
-          <Route path="/blogTwo" element={<BlogTwo/>}/>
-          <Route path="/blogThree" element={<BlogThree/>}/>
-          <Route path="/socialMediaOne" element={<SocialMeadiaOne/>}/>
-          <Route path="/socialMediaTwo" element={<SocialMediaTwo/>}/>
+          <Route path="/blogOne" element={<BlogOne />} />
+          <Route path="/blogTwo" element={<BlogTwo />} />
+          <Route path="/blogThree" element={<BlogThree />} />
+          <Route path="/socialMediaOne" element={<SocialMeadiaOne />} />
+          <Route path="/socialMediaTwo" element={<SocialMediaTwo />} />
         </Routes>
       </main>
     </div>
