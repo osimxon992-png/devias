efff226bbc7c41101293d3527c299a2487f4fdd4
Osimxon <osimhondadaxanov2013@gmail.com>
2026-09-17 14:09:32 +0500

    changed color

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <aside>
         <EditedSidebar />
       </aside>
-      <main className="flex-1 bg-[#fff]">
+      <main className="flex-1 bg-[#FFFFFF]">
         <Header />
         <Routes>
           <Route path="/" element={<Account />} />
