023fe85ab1ba5f2a82c677ba630ed516093df127
mirmakhmud5051 <mirmahmudjunaydullayev@gmail.com>
2026-09-22 15:32:33 +0500

    update project

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ThemeProvider, CssBaseline } from "@mui/material";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import theme from "./charts/theme";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const rootElement = document.getElementById("root");
-
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </React.StrictMode>,
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
